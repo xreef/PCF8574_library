b6fa26e50de4d0525498c3caabeb6c0e6cdf6d15
Renzo Mischianti <renzo.mischianti@gmail.com>
2020-05-20 21:47:15 +0200

    Fix LICENCE

--- PCF8574.h
+++ PCF8574.h
@@ -1,10 +1,17 @@
 /*
  * PCF8574 GPIO Port Expand
+ *
+ * AUTHOR:  Renzo Mischianti
+ * VERSION: 2.2.0
+ *
  * https://www.mischianti.org/2019/01/02/pcf8574-i2c-digital-i-o-expander-fast-easy-usage/
  *
  * The MIT License (MIT)
  *
  * Copyright (c) 2017 Renzo Mischianti www.mischianti.org All right reserved.
+ *
+ * You may copy, alter and reuse this code in any way you like, but please leave
+ * reference to www.mischianti.org in your comments if you redistribute this code.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
