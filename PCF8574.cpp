f8337c085402358f05d991180d936235f8ba903d
Renzo Mischianti <renzo.mischianti@gmail.com>
2025-10-30 09:45:49 +0100

    Implement Poki encoder reading algorithm and enhance buffer reading logic

--- PCF8574.cpp
+++ PCF8574.cpp
@@ -684,7 +684,19 @@ bool PCF8574::checkProgression(byte oldValA, byte oldValB, byte newValA, byte ne
 //		  byte encoderPinALast = (this->encoderValues & bit(pinA))>0?HIGH:LOW;
 //		  byte encoderPinBLast = (this->encoderValues & bit(pinB))>0?HIGH:LOW;
 //
-//		  if ((encoderPinALast!=na || encoderPinBLast!=nb) && ((encoderPinALast == LOW) || encoderPinALast==encoderPinBLast) && (na == HIGH)) {
+////		  Serial.print(pinA);
+////		  Serial.print(" TO --> ");
+////		  Serial.print(encoderPinALast);
+////		  Serial.print(encoderPinBLast);
+////		  Serial.print(" - ");
+////		  Serial.print(na);
+////		  Serial.print(nb);
+//
+//		  if (
+//
+//		  ((encoderPinALast!=na || encoderPinBLast!=nb) && ((encoderPinALast == LOW) || encoderPinALast==encoderPinBLast) && (na == HIGH))
+//		  || ((encoderPinALast!=na || encoderPinBLast!=nb) && ((encoderPinALast == HIGH) || encoderPinALast==encoderPinBLast) && (na == LOW))
+//			){
 //			  DEBUG_PRINT("TO --> ");
 //			  DEBUG_PRINT(encoderPinALast);
 //			  DEBUG_PRINT(encoderPinBLast);
@@ -692,6 +704,8 @@ bool PCF8574::checkProgression(byte oldValA, byte oldValB, byte newValA, byte ne
 //			  DEBUG_PRINT(na);
 //			  DEBUG_PRINT(nb);
 //			  DEBUG_PRINTLN();
+//
+////			  Serial.print (" <------ ");
 //
 //				if (nb == LOW && nb!=na) {
 //					*encoderValue = *encoderValue + (!reverseRotation?+1:-1);
@@ -701,5 +715,378 @@ bool PCF8574::checkProgression(byte oldValA, byte oldValB, byte newValA, byte ne
 //					changed = true;
 //				}
 //		  }
+////		  Serial.println();
+////		  encoderValues = encoderValues & (~(bit(pinA) | bit(pinB)));
+////		  if (na == HIGH){
+////			  encoderValues = encoderValues | bit(pinA);
+////		  }
+////		  if (nb == HIGH){
+////			  encoderValues = encoderValues | bit(pinA);
+////		  }
 //
+//		  if (encoderPinALast!=na || encoderPinBLast!=nb){
+//		  this->encoderValues = (encoderPinALast!=na)?this->encoderValues ^ bit(pinA):this->encoderValues;
+//		  this->encoderValues = (encoderPinBLast!=nb)?this->encoderValues ^ bit(pinB):this->encoderValues;
+//		  }
+//
+//		  PCF8574::attachInterrupt();
+//			return changed;
+//	}
+//	int8_t PCF8574::readEncoderValueEvolved(uint8_t pinA, uint8_t pinB) {
+//		volatile long encoderValue = 0;
+//		PCF8574::readEncoderValueEvolved(pinA, pinB, &encoderValue);
+//		return encoderValue;
+//	}
+//
+//#endif
+
+#ifdef POKI_ENCODER_ALGORITHM
+	bool PCF8574::readEncoderValuePoki(uint8_t pinA, uint8_t pinB, volatile long *encoderValue, bool reverseRotation){
+		  PCF8574::detachInterrupt();
+
+		  bool changed = false;
+
+		  byte na = PCF8574::digitalRead(pinA, true);
+		  byte nb = PCF8574::digitalRead(pinB, true);
+
+		  byte encoderPinALast = (this->encoderValues & bit(pinA))>0?HIGH:LOW;
+		  byte encoderPinBLast = (this->encoderValues & bit(pinB))>0?HIGH:LOW;
+
+		  DEBUG_PRINT("TO --> ");
+		  DEBUG_PRINT(encoderPinALast);
+		  DEBUG_PRINT(encoderPinBLast);
+		  DEBUG_PRINT(" - ");
+		  DEBUG_PRINT(na);
+		  DEBUG_PRINT(nb);
+		  DEBUG_PRINTLN();
+
+		  if ((encoderPinALast!=na || encoderPinBLast!=nb) && ((encoderPinALast == LOW) || encoderPinALast==encoderPinBLast) && (na == HIGH)) {
+			  DEBUG_PRINT("TO --> ");
+			  DEBUG_PRINT(encoderPinALast);
+			  DEBUG_PRINT(encoderPinBLast);
+			  DEBUG_PRINT(" - ");
+			  DEBUG_PRINT(na);
+			  DEBUG_PRINT(nb);
+			  DEBUG_PRINTLN();
+
+			  if (na && !nb) {
+					if (encoderPinBLast) {
+						*encoderValue = *encoderValue + (!reverseRotation?+1:-1);
+					} else {
+						*encoderValue = *encoderValue  + (!reverseRotation?-1:+1);
+					}
+					changed = true;
+				}
+		  }
+
+		  this->encoderValues = (encoderPinALast!=na)?this->encoderValues ^ bit(pinA):encoderValues;
+		  this->encoderValues = (encoderPinBLast!=nb)?this->encoderValues ^ bit(pinB):encoderValues;
+		  PCF8574::attachInterrupt();
+
+			return changed;
+	}
+	int8_t PCF8574::readEncoderValuePoki(uint8_t pinA, uint8_t pinB) {
+		volatile long encoderValue = 0;
+		PCF8574::readEncoderValue(pinA, pinB, &encoderValue);
+		return encoderValue;
+	}
+
+#endif
+
+
+/**
+ * Read value from i2c and bufferize it
+ * @param force
+ */
+void PCF8574::readBuffer(bool force){
+//	if (millis() > PCF8574::lastReadMillis+latency || _usingInterrupt || force){
+	if (millis() - PCF8574::lastReadMillis > latency || _usingInterrupt || force){
+		_wire->requestFrom(_address,(uint8_t)1);// Begin transmission to PCF8574 with the buttons
+		lastReadMillis = millis();
+		if(_wire->available())   // If bytes are available to be recieved
+		{
+			byte iInput = _wire->read();// Read a byte
+			  if ((iInput & readModePullDown)>0 and (~iInput & readModePullUp)>0){
+//			  if ((iInput & readMode)>0){
+				byteBuffered = (byteBuffered & ~readMode) | (byte)iInput;
+			}
+		}
+	}
+}
+
+#ifndef PCF8574_LOW_MEMORY
+	/**
+	 * Read value of all INPUT pin
+	 * Debounce read more fast than 10millis, non managed for interrupt mode
+	 * @return
+	 */
+	PCF8574::DigitalInput PCF8574::digitalReadAll(void){
+		DEBUG_PRINTLN("Read from buffer");
+		_wire->requestFrom(_address,(uint8_t)1);// Begin transmission to PCF8574 with the buttons
+		lastReadMillis = millis();
+		if(_wire->available())   // If bytes are available to be recieved
+		{
+			  DEBUG_PRINTLN("Data ready");
+			  byte iInput = _wire->read();// Read a byte
+
+			  if ((readModePullDown & iInput)>0 or (readModePullUp & ~iInput)>0){
+				  DEBUG_PRINT(" -------- CHANGE --------- ");
+				  byteBuffered = (byteBuffered & ~readMode) | (byte)iInput;
+			  }
+		}
+
+		DEBUG_PRINT("Buffer value ");
+		DEBUG_PRINTLN(byteBuffered, BIN);
+
+		if ((bit(0) & readMode)>0) digitalInput.p0 = ((byteBuffered & bit(0))>0)?HIGH:LOW;
+		if ((bit(1) & readMode)>0) digitalInput.p1 = ((byteBuffered & bit(1))>0)?HIGH:LOW;
+		if ((bit(2) & readMode)>0) digitalInput.p2 = ((byteBuffered & bit(2))>0)?HIGH:LOW;
+		if ((bit(3) & readMode)>0) digitalInput.p3 = ((byteBuffered & bit(3))>0)?HIGH:LOW;
+		if ((bit(4) & readMode)>0) digitalInput.p4 = ((byteBuffered & bit(4))>0)?HIGH:LOW;
+		if ((bit(5) & readMode)>0) digitalInput.p5 = ((byteBuffered & bit(5))>0)?HIGH:LOW;
+		if ((bit(6) & readMode)>0) digitalInput.p6 = ((byteBuffered & bit(6))>0)?HIGH:LOW;
+		if ((bit(7) & readMode)>0) digitalInput.p7 = ((byteBuffered & bit(7))>0)?HIGH:LOW;
+
+		if ((bit(0) & writeMode)>0) digitalInput.p0 = ((writeByteBuffered & bit(0))>0)?HIGH:LOW;
+		if ((bit(1) & writeMode)>0) digitalInput.p1 = ((writeByteBuffered & bit(1))>0)?HIGH:LOW;
+		if ((bit(2) & writeMode)>0) digitalInput.p2 = ((writeByteBuffered & bit(2))>0)?HIGH:LOW;
+		if ((bit(3) & writeMode)>0) digitalInput.p3 = ((writeByteBuffered & bit(3))>0)?HIGH:LOW;
+		if ((bit(4) & writeMode)>0) digitalInput.p4 = ((writeByteBuffered & bit(4))>0)?HIGH:LOW;
+		if ((bit(5) & writeMode)>0) digitalInput.p5 = ((writeByteBuffered & bit(5))>0)?HIGH:LOW;
+		if ((bit(6) & writeMode)>0) digitalInput.p6 = ((writeByteBuffered & bit(6))>0)?HIGH:LOW;
+		if ((bit(7) & writeMode)>0) digitalInput.p7 = ((writeByteBuffered & bit(7))>0)?HIGH:LOW;
+
+		//if ((byteBuffered & readModePullDown)>0 and (~byteBuffered & readModePullUp)>0){
+
+//		byteBuffered = (resetInitial & readMode) | (byteBuffered  & ~readMode); //~readMode & byteBuffered;
+
+		byteBuffered = (initialBuffer & readMode) | (byteBuffered  & ~readMode); //~readMode & byteBuffered;
+
+			DEBUG_PRINT("Buffer hight value readed set readed ");
+			DEBUG_PRINTLN(byteBuffered, BIN);
+		//}
+		DEBUG_PRINT("Return value ");
+		return digitalInput;
+	};
+#else
+	/**
+	 * Read value of all INPUT pin in byte format for low memory usage
+	 * Debounce read more fast than 10millis, non managed for interrupt mode
+	 * @return
+	 */
+	byte PCF8574::digitalReadAll(void){
+		DEBUG_PRINTLN("Read from buffer");
+		_wire->requestFrom(_address,(uint8_t)1);// Begin transmission to PCF8574 with the buttons
+		lastReadMillis = millis();
+		if(_wire->available())   // If bytes are available to be recieved
+		{
+			  DEBUG_PRINTLN("Data ready");
+			  byte iInput = _wire->read();// Read a byte
+
+			  if ((readModePullDown & iInput)>0 or (readModePullUp & ~iInput)>0){
+				  DEBUG_PRINT(" -------- CHANGE --------- ");
+				  byteBuffered = (byteBuffered & ~readMode) | (byte)iInput;
+
+			  }
+		}
+
+		DEBUG_PRINT("Buffer value ");
+		DEBUG_PRINTLN(byteBuffered, BIN);
+
+		byte byteRead = byteBuffered | writeByteBuffered;
+
+		//if ((byteBuffered & readModePullDown)>0 and (~byteBuffered & readModePullUp)>0){
+//			byteBuffered = (resetInitial & readMode) | (byteBuffered  & ~readMode); //~readMode & byteBuffered;
+		byteBuffered = (initialBuffer & readMode) | (byteBuffered  & ~readMode); //~readMode & byteBuffered;
+			DEBUG_PRINT("Buffer hight value readed set readed ");
+			DEBUG_PRINTLN(byteBuffered, BIN);
+		//}
+		DEBUG_PRINT("Return value ");
+		return byteRead;
+	};
+#endif
+
+/**
+ * Read value of specified pin
+ * Debounce read more fast than 10millis, non managed for interrupt mode
+ * @param pin
+ * @return
+ */
+uint8_t PCF8574::digitalRead(uint8_t pin, bool forceReadNow){
+	uint8_t value = (bit(pin) & readModePullUp)?HIGH:LOW;
+	DEBUG_PRINT("Read pin ");
+	DEBUG_PRINT (pin);
+	// Check if pin already HIGH than read and prevent reread of i2c
+//	DEBUG_PRINTLN("----------------------------------")
+//	DEBUG_PRINT("readModePullUp   ");
+//	DEBUG_PRINTLN(readModePullUp, BIN);
+//	DEBUG_PRINT("readModePullDown ");
+//	DEBUG_PRINTLN(readModePullDown, BIN);
+//	DEBUG_PRINT("byteBuffered     ");
+//	DEBUG_PRINTLN(byteBuffered, BIN);
+
+
+	if ((((bit(pin) & (readModePullDown & byteBuffered))>0) or (bit(pin) & (readModePullUp & ~byteBuffered))>0 )){
+		DEBUG_PRINTLN(" ...Pin already set");
+		  if ((bit(pin) & byteBuffered)>0){
+			  value = HIGH;
+		  }else{
+			  value = LOW;
+		  }
+//	 }else if (forceReadNow || (millis() > PCF8574::lastReadMillis+latency)){
+	 }else if (forceReadNow || (millis() - PCF8574::lastReadMillis > latency)){
+		 DEBUG_PRINT(" ...Read from buffer... ");
+		  _wire->requestFrom(_address,(uint8_t)1);// Begin transmission to PCF8574 with the buttons
+		  lastReadMillis = millis();
+		  if(_wire->available())   // If bytes are available to be recieved
+		  {
+			  DEBUG_PRINTLN(" Data ready");
+			  byte iInput = _wire->read();// Read a byte
+			  DEBUG_PRINT("Input ");
+			  DEBUG_PRINT((byte)iInput, BIN);
+
+			  if ((readModePullDown & iInput)>0 or (readModePullUp & ~iInput)>0){
+				  DEBUG_PRINT(" -------- CHANGE --------- ");
+				  byteBuffered = (byteBuffered & ~readMode) | (byte)iInput;
+				  if ((bit(pin) & byteBuffered)>0){
+					  value = HIGH;
+				  }else{
+					  value = LOW;
+				  }
+//				  value = (bit(pin) & byteBuffered);
+			  }
+		  }
+	}
+	DEBUG_PRINT(" ..Buffer value ");
+	DEBUG_PRINT(byteBuffered, BIN);
+	// If HIGH set to low to read buffer only one time
+	if ((bit(pin) & readModePullDown) and value==HIGH){
+		byteBuffered = bit(pin) ^ byteBuffered;
+		DEBUG_PRINT(" ...Buffer hight value readed set readed ");
+		DEBUG_PRINT (byteBuffered, BIN);
+	}else if ((bit(pin) & readModePullUp) and value==LOW){
+		byteBuffered = bit(pin) ^ byteBuffered;
+		DEBUG_PRINT(" ...Buffer low value readed set readed ");
+		DEBUG_PRINT(byteBuffered, BIN);
+	}else if(bit(pin) & writeByteBuffered){
+		value = HIGH;
+	}
+	DEBUG_PRINT(" ...Return value ");
+	DEBUG_PRINTLN(value);
+	return value;
+};
+
+/**
+ * Write on pin
+ * @param pin
+ * @param value
+ */
+bool PCF8574::digitalWrite(uint8_t pin, uint8_t value){
+	DEBUG_PRINTLN("Begin trasmission");
+	_wire->beginTransmission(_address);     //Begin the transmission to PCF8574
+	DEBUG_PRINT("Value ");
+	DEBUG_PRINT(value);
+	DEBUG_PRINT(" Write data pre ");
+	DEBUG_PRINT(writeByteBuffered, BIN);
+
+	if (value==HIGH){
+		writeByteBuffered = writeByteBuffered | bit(pin);
+		byteBuffered  = writeByteBuffered | bit(pin);
+	}else{
+		writeByteBuffered = writeByteBuffered & ~bit(pin);
+		byteBuffered  = writeByteBuffered & ~bit(pin);
+	}
+	DEBUG_PRINT("Write data ");
+	DEBUG_PRINT(writeByteBuffered, BIN);
+	DEBUG_PRINT(" for pin ");
+	DEBUG_PRINT(pin);
+	DEBUG_PRINT(" bin value ");
+	DEBUG_PRINT(bit(pin), BIN);
+	DEBUG_PRINT(" value ");
+	DEBUG_PRINT(value);
+
+	// writeByteBuffered = writeByteBuffered & (~writeMode & byteBuffered);
+	byteBuffered = (writeByteBuffered & writeMode) | (resetInitial & readMode);
+
+	// byteBuffered = (writeByteBuffered & writeMode) | (byteBuffered & readMode);
+	DEBUG_PRINT(" byteBuffered ");
+	DEBUG_PRINTLN(byteBuffered, BIN);
+
+	DEBUG_PRINT("Going to write data ");
+	DEBUG_PRINTLN(writeByteBuffered, BIN);
+
+	_wire->write(byteBuffered);
+
+	byteBuffered = (writeByteBuffered & writeMode) | (initialBuffer & readMode);
+
+//	byteBuffered = (writeByteBuffered & writeMode) & (byteBuffered & readMode);
+	DEBUG_PRINTLN("Start end trasmission if stop here check pullup resistor.");
+
+	this->transmissionStatus = _wire->endTransmission();
+
+	return this->isLastTransmissionSuccess();
+};
+
+#ifndef PCF8574_LOW_MEMORY
+	/**
+	 * Read value of all INPUT pin
+	 * Debounce read more fast than 10millis, non managed for interrupt mode
+	 * @return
+	 */
+	void PCF8574::setVal(uint8_t pin, uint8_t value){
+		if (value==HIGH){
+			writeByteBuffered = writeByteBuffered | bit(pin);
+			byteBuffered  = writeByteBuffered | bit(pin);
+		}else{
+			writeByteBuffered = writeByteBuffered & ~bit(pin);
+			byteBuffered  = writeByteBuffered & ~bit(pin);
+		}
+
+	}
+	bool PCF8574::digitalWriteAll(PCF8574::DigitalInput digitalInput){
+
+		setVal(P0, digitalInput.p0);
+		setVal(P1, digitalInput.p1);
+		setVal(P2, digitalInput.p2);
+		setVal(P3, digitalInput.p3);
+		setVal(P4, digitalInput.p4);
+		setVal(P5, digitalInput.p5);
+		setVal(P6, digitalInput.p6);
+		setVal(P7, digitalInput.p7);
+
+		return digitalWriteAllBytes(writeByteBuffered);
+	}
+#else
+	bool PCF8574::digitalWriteAll(byte digitalInput){
+		return digitalWriteAllBytes(digitalInput);
+	}
+#endif
+
+
+bool PCF8574::digitalWriteAllBytes(byte allpins){
+	_wire->beginTransmission(_address);     //Begin the transmission to PCF8574
+
+	// writeByteBuffered = writeByteBuffered & (~writeMode & byteBuffered);
+	writeByteBuffered = allpins;
+	byteBuffered = (writeByteBuffered & writeMode) | (resetInitial & readMode);
+
+	// byteBuffered = (writeByteBuffered & writeMode) | (byteBuffered & readMode);
+	DEBUG_PRINT(" byteBuffered ");
+	DEBUG_PRINTLN(byteBuffered, BIN);
+
+	DEBUG_PRINT("Going to write data ");
+	DEBUG_PRINTLN(writeByteBuffered, BIN);
+
+	_wire->write(byteBuffered);
+
+	byteBuffered = (writeByteBuffered & writeMode) | (initialBuffer & readMode);
+
+//	byteBuffered = (writeByteBuffered & writeMode) & (byteBuffered & readMode);
+	DEBUG_PRINTLN("Start end trasmission if stop here check pullup resistor.");
+
+	this->transmissionStatus = _wire->endTransmission();
+
+	return this->isLastTransmissionSuccess();
+
+}
 
